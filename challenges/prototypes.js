c8467431d6397a7d2f8ffc0e981967084075abea
devin-bielejec <devin.bieleje@gmail.com>
2019-07-26 12:26:21 -0400

    Checked prototypes

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -40,7 +40,6 @@ CuboidMaker.prototype.surfaceArea = function() {
 */
 
 const cuboid = new CuboidMaker({length: 4, width: 5, height: 5});
-console.log(cuboid)
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 console.log(cuboid.volume()); // 100
